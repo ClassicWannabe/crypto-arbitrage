5be4078a4de7a02fecb1825a1c2fe3fbeff3c09f
ClassicWannabe <ruslaneleusinov@gmail.com>
2024-01-10 11:40:03 +0500

    Add ccxt lib

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+import ccxt from 'ccxt'
+import fs from 'fs'
+
+const binance = new ccxt.binance()
+const kraken = new ccxt.kraken()
+
 export async function main() {
-    
+    await binance.loadMarkets()
+    await kraken.loadMarkets()
+
+    console.log(process.env)
+
+    fs.writeFileSync('temp/binance-markets.json', JSON.stringify(binance.markets, null, 2))
+    fs.writeFileSync('temp/kraken-markets.json', JSON.stringify(kraken.markets, null, 2))
 }
+
+main()
