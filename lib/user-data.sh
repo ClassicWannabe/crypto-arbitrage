ec006cfad1b62c0c75045c7fa9c21f0ffac163b5
Ruslan Yeleussinov <ruslan.yeleussinov@carrier.com>
2024-01-17 02:25:41 +0500

    Fix EC2 user data script

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -9,15 +9,15 @@ sudo ln -s /usr/bin/npm-20 /usr/bin/npm
 
 npm install -g yarn
 
-sudo mkdir /home/ec2-user/arbitrage-app
+sudo mkdir -p /home/my-user/arbitrage-app
 
-sudo aws ssm get-parameter --with-decryption --name /github/deploy-key --output text --query Parameter.Value > /home/ec2-user/.ssh/id_ed25519_arbitrage_server
+sudo aws ssm get-parameter --with-decryption --name /github/deploy-key --output text --query Parameter.Value > /home/my-user/.ssh/id_ed25519_arbitrage_server
 
-sudo git clone git@github.com:ClassicWannabe/crypto-arbitrage.git /home/ec2-user/arbitrage-app/
+sudo git clone git@github.com:ClassicWannabe/crypto-arbitrage.git /home/my-user/arbitrage-app/
 
-sudo aws ssm get-parameter --with-decryption --name /crypto-arbitrage/env --output text --query Parameter.Value > /home/ec2-user/arbitrage-app/.env
+sudo aws ssm get-parameter --with-decryption --name /crypto-arbitrage/env --output text --query Parameter.Value > /home/my-user/arbitrage-app/.env
 
-sudo cd /home/ec2-user/arbitrage-app
+sudo cd /home/my-user/arbitrage-app
 
 sudo yarn install --prod
 
