dd7d9b5baca316739050c22d7cc432653e2f00be
Ruslan Yeleussinov <ruslan.yeleussinov@carrier.com>
2024-01-21 15:38:38 +0500

    Fix template path

--- src/formatters/ArbitrageFormatter/consts.ts
+++ src/formatters/ArbitrageFormatter/consts.ts
@@ -4,8 +4,6 @@ import path from "path";
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export const templatesPath = path.resolve(
-  __dirname + "../../../../templates/table.hbs"
-);
+export const templatesPath = path.resolve(__dirname + "../../../../templates/");
 
 export const tableHbsPath = path.resolve(templatesPath + "/table.hbs");
